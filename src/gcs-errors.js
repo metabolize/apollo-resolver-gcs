8bb1c1ed31270cacbfcd3b447df0b344ebef2d69
Paul Melnikow <github@paulmelnikow.com>
2018-11-16 18:47:31 -0500

    Set up CircleCI (#1)

--- src/gcs-errors.js
+++ src/gcs-errors.js
@@ -4,10 +4,9 @@ function isErrorReason(e, reason) {
   if ('errors' in e) {
     return e.errors.length === 1 && e.errors[0].reason === reason
   } else {
-    throw Error(`
-      This is not an API error response. It may be an HTTP response. In that
-      case you should inspect its \`code\` property.
-    `)
+    // This is not an API error response. It may be an HTTP response. In that
+    // case you should inspect its \`code\` property.
+    throw e
   }
 }
 
